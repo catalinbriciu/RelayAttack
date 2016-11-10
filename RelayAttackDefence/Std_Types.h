6b852e7b6a44b4f4fc19a5eb36b9c5e7004f1e38
Catalin Briciu <briciucatalin@yahoo.com>
2016-11-10 22:17:12 +0200

    FirstVersion

---
+++ RelayAttackDefence/Std_Types.h
@@ -0,0 +1,15 @@
+#ifndef STD_TYPES_H
+
+#define STD_TYPES_H
+//insert here the code
+//value for replacing the boolean TRUE
+#define TRUE 1u
+//value for replacing the boolean FALSE
+#define FALSE 0u
+
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned int uint32;
+
+
+#endif //STD_TYPES_H
